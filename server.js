9ed4cd93df208227efe5ea1fa178c64911c424a2
efrenjr-dev <efrenjr.rodiguez@gmail.com>
2021-12-17 14:07:58 +0300

    updated get and post routes

--- server.js
+++ server.js
@@ -12,18 +12,13 @@ let payload = [];
 // require('./app/routes')(app, {});
 
 app.get("/", (req, res) => {
-  console.log(req.body);
   console.log(payload[payload.length - 1]);
   return res.send(payload[payload.length - 1]);
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
   payload.push(req.body);
   return res.send("webhook received");
-  //   return res.send({
-  //     error: "Bad Request : missing required parameter NAME",
-  //   });
 });
 
 app.listen(PORT, () => {
